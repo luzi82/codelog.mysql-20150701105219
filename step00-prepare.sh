e2ac17b12b690087ce52658ddd6b69909c6046d5
Luzi82 <luzi82@gmail.com>
2015-07-01 13:04:50 +0800

    code

--- step00-prepare.sh
+++ step00-prepare.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 . ./_conf.sh
 
 rm -rf tmp
 mkdir tmp
 
-echo "a,b" >> tmp/data.csv
+echo "c0,c1" >> tmp/data.csv
 
 for a in {0..99}
 do
@@ -15,4 +17,6 @@ do
 	done
 done
 
-${MYSQL_CMD} -e "DROP TABLE t0"
+${MYSQL_CMD} -e "DROP TABLE t0" || echo ""
+${MYSQL_CMD} -e "CREATE TABLE t0(pk int(11) NOT NULL AUTO_INCREMENT,c0 int(11),c1 int(11),PRIMARY KEY(pk),KEY idx0(c0),KEY idx1(c1),KEY idx01(c0,c1),KEY idx10(c1,c0))"
+${MYSQL_CMD} -e "LOAD DATA INFILE tmp/data.csv INTO t0"
