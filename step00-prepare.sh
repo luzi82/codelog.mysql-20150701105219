2ec84a4585bba29a505bbd4f0cfe401a3c4aadde
Luzi82 <luzi82@gmail.com>
2015-07-01 13:06:05 +0800

    code

--- step00-prepare.sh
+++ step00-prepare.sh
@@ -19,4 +19,4 @@ done
 
 ${MYSQL_CMD} -e "DROP TABLE t0" || echo ""
 ${MYSQL_CMD} -e "CREATE TABLE t0(pk int(11) NOT NULL AUTO_INCREMENT,c0 int(11),c1 int(11),PRIMARY KEY(pk),KEY idx0(c0),KEY idx1(c1),KEY idx01(c0,c1),KEY idx10(c1,c0))"
-${MYSQL_CMD} -e "LOAD DATA INFILE "tmp/data.csv" INTO t0"
+${MYSQL_CMD} -e "LOAD DATA INFILE "tmp/data.csv" INTO TABLE t0"
